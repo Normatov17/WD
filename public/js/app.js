adb855f403d2917d289103adc70727e3d9cc8410
DavlatbekNormatov <davlatbeknormatov17@gmail.com>
2023-03-22 23:09:34 +0500

    ddd

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,9 @@
 AOS.init();
+
+// media
+const menuHumburger = document.querySelector(".menu-burger");
+const menuLinks = document.querySelector(".menu-Links");
+
+menuHumburger.addEventListener("click", (e) => {
+    menuLinks.classList.toggle("open");
+});
